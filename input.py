f5e2a83417eb9b3d530eecf43551a78ccd3e0e4d
williamBMDK <williambillemeyling@gmail.com>
2021-01-15 01:03:34 +0100

    input in python

---
+++ input.py
@@ -0,0 +1,14 @@
+# sådan kan man læse et tal i python
+n = int(input())
+
+# sådan kan man læse 2 tal i python
+a,b = map(int,input().split())
+
+# sådan kan man læse en liste med tal i python
+arr = list(map(int,input().split()))
+
+# sådan kan man læse en tekstren i python (hele linjen)
+linje = input()
+
+# sådan kan man læse en liste med tekststrenge  i python (mellemrumssepererede strings)
+strings = input().split()
