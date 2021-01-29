32e36ac9928916611487e3a7d18c32e6c3754ebe
chri80d2 <44302442+chri80d2@users.noreply.github.com>
2021-01-29 02:41:21 +0100

    flyingsafely

---
+++ 29_1_2021/flyingsafely/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define print(x) cout << x << endl
+#define debug(x) cerr << #x << " : " << x << endl
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m; cin >> n >> m;
+        for (int i = 0; i < m; i++) {
+            int whatever, whocares; cin >> whatever >> whocares;
+        }
+
+        print(n-1);
+    }
+
+	return 0;
+}
